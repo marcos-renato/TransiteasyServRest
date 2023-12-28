d38d3eeb32f2cf9cba2621761d4e639faf9ab6a8
marcos-renato <marcos.vicente@mrpv.org>
2023-12-28 12:24:38 -0300

    correçẽs nas dependencias

--- src/models/db.js
+++ src/models/db.js
@@ -4,10 +4,10 @@ const PORTDB=process.env.PORTDBSECRET;
 const USERNAMEDB=process.env.USERNAMEDBSECRET;
 const PASSWORDDB=process.env.PASSWORDDBSECRET;
 const DATABASEDB=process.env.DATABASEDBSECRET;
-/*const DIALECTDB=process.env.DIALECTDBSECRET;*/
+const DIALECTDB=process.env.DIALECTDBSECRET;
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize({
-  dialect: mysql, 
+  dialect: DIALECTDB, 
   host: HOSTDB, 
   port: PORTDB,
   username: USERNAMEDB,
